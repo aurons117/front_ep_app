a36fe05e60ed23421165a54482a979d92e71498e
Alexis Sandin <icarbajals0600@gmail.com>
2021-01-28 21:31:23 -0600

    url actual para hacer build

--- data/environment.js
+++ data/environment.js
@@ -1,4 +1,5 @@
 
-const environmentURL = 'http://localhost:1337';
+// const environmentURL = 'http://localhost:1337';
+const environmentURL = 'http://18.216.72.86:1337';
 
 export default environmentURL;
