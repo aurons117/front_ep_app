1693f2a09d98754101a2f34aeab86a1250aa5128
Alexis Sandin <icarbajals0600@gmail.com>
2021-06-11 13:48:30 -0500

    PWA implementado

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '../styles/globals.scss';
 import React from 'react';
 import animate from 'animate.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp;
